2efa45cf3d43306bf83c49341c5493265d358d61
Dan Gilbert <ddg@daentech.co.uk>
2012-01-06 16:37:30 +0000

    Fixed permissions to add internet and location permissions. Added MapView to main activity

--- src/uk/co/daentech/citythrow/CityThrowActivity.java
+++ src/uk/co/daentech/citythrow/CityThrowActivity.java
@@ -1,13 +1,26 @@
 package uk.co.daentech.citythrow;
 
-import android.app.Activity;
 import android.os.Bundle;
 
-public class CityThrowActivity extends Activity {
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
+public class CityThrowActivity extends MapActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        MapView mv = (MapView)findViewById(R.id.mapview);
+        
+        mv.setBuiltInZoomControls(true);
+        
     }
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
