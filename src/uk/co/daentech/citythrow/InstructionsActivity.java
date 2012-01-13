442f7fa78c4e1e63d9feb0dc239717621f8d71c9
Dan Gilbert <ddg@daentech.co.uk>
2012-01-13 11:00:42 +0000

    Added instructions layout. Haven't looked yet, so need to test it

--- src/uk/co/daentech/citythrow/InstructionsActivity.java
+++ src/uk/co/daentech/citythrow/InstructionsActivity.java
@@ -1,7 +1,15 @@
 package uk.co.daentech.citythrow;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class InstructionsActivity extends Activity {
+	
+	@Override
+	public void onCreate(Bundle bundle){
+		super.onCreate(bundle);
+		
+        setContentView(R.layout.instructions);
+	}
 
 }
