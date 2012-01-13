31138340e941320b4e752b523bf990ca0b93d800
Dan Gilbert <ddg@daentech.co.uk>
2012-01-13 12:19:14 +0000

    Added resources for the menu and made the menu look right and be clickable

--- src/uk/co/daentech/citythrow/InstructionsActivity.java
+++ src/uk/co/daentech/citythrow/InstructionsActivity.java
@@ -2,14 +2,31 @@ package uk.co.daentech.citythrow;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class InstructionsActivity extends Activity {
+public class InstructionsActivity extends Activity implements OnClickListener{
 	
 	@Override
 	public void onCreate(Bundle bundle){
 		super.onCreate(bundle);
 		
         setContentView(R.layout.instructions);
+        
+        Button btnDone = (Button)findViewById(R.id.btnDone);
+        btnDone.setOnClickListener(this);
+	}
+
+	public void onClick(View v) {
+		switch(v.getId()){
+		case R.id.btnDone:
+			this.finish();
+			break;
+		default:
+			break;
+		}
+		
 	}
 
 }
--- src/uk/co/daentech/citythrow/MainMenuActivity.java
+++ src/uk/co/daentech/citythrow/MainMenuActivity.java
@@ -1,7 +1,45 @@
 package uk.co.daentech.citythrow;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
-public class MainMenuActivity extends Activity {
+public class MainMenuActivity extends Activity implements OnClickListener{
+	
+	
+	@Override
+	public void onCreate(Bundle bundle){
+		super.onCreate(bundle);
+		setContentView(R.layout.mainmenu);
+		
+		ImageView iv = (ImageView)findViewById(R.id.start);
+		iv.setOnClickListener(this);
+		iv = (ImageView)findViewById(R.id.instructions);
+		iv.setOnClickListener(this);
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu){
+		super.onCreateOptionsMenu(menu);
+		
+		return false;
+	}
+	
+	public void onClick(View v) {
+		switch (v.getId()){
+		case R.id.start:
+			startActivity(new Intent(this,CityThrowActivity.class));
+			break;
+		case R.id.instructions:
+			startActivity(new Intent(this,InstructionsActivity.class));
+			break;
+		default:
+			break;
+		}
+		
+	}
 }
